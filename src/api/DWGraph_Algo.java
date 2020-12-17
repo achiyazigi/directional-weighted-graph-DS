7eadaafb2a34512c67f7bc37d64d89fa49f627d7
RavidSaadia <ravids1233@gmail.com>
2020-12-18 00:03:26 +0200

    save fixed

--- src/api/DWGraph_Algo.java
+++ src/api/DWGraph_Algo.java
@@ -1,6 +1,10 @@
 package api;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import java.io.*;
 import java.util.*;
 
@@ -8,41 +12,21 @@ public class DWGraph_Algo implements dw_graph_algorithms {
 
     private directed_weighted_graph _g;
 
-    
-    /**
-     * init target graph
-     * @param g
-     */
     @Override
     public void init(directed_weighted_graph g) {
         _g = g;
     }
 
-    
-    /** 
-     * returns the initialized graph
-     * @return directed_weighted_graph
-     */
     @Override
     public directed_weighted_graph getGraph() {
         return _g;
     }
 
-    
-    /** 
-     * @return directed_weighted_graph
-     */
     @Override
     public directed_weighted_graph copy() {
         return new DWGraph_DS(_g);
     }
 
-    
-    /** 
-     * running BFS but each edge u,v we check if there's a path back from v to u
-     * (not necessarily the shortest one)
-     * @return boolean
-     */
     @Override
     public boolean isConnected() {
         if (_g.nodeSize() == 0 || _g.nodeSize() == 1) {
@@ -51,23 +35,23 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         if (this._g.nodeSize() > this._g.edgeSize() + 1) { //the minimum number of edges for a connected graph, shallow chek.
             return false;
         }
-        if (reset_and_lonely_check()) {//reset all the Tags in the graph to -1 and check if there is a lonely node.
+        if (reset_and_lonely_check()) {//reset all the Tags in the graph to 0 and check if there is a lonely node.
             return false;            // if there is a lonely node return false immediately.
         }
 
-        Queue<node_data> q = new LinkedList<>();
+        Queue<node_data> q = new LinkedList<>(); // Queue to store all the checked Nodes.
         q.add(_g.getV().iterator().next());
         int counter = 0;
         node_data cur = q.peek();
         cur.setTag(0);
-        
+
         while (!q.isEmpty()) {
             cur = q.poll();
             counter++;
-            for (edge_data i : _g.getE(cur.getKey())) {
+            for (edge_data i : _g.getE(cur.getKey())) { //check if V neighbor has been checked.
                 node_data dest = _g.getNode(i.getDest());
                 if (dest.getTag() == -1) {
-                    if(!path(dest.getKey(),cur.getKey())){
+                    if (!path(dest.getKey(), cur.getKey())) {
                         return false;
                     }
                     dest.setTag(0);
@@ -78,29 +62,21 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         return _g.getV().size() == counter;
     }
 
-    
-    /**
-     * returns true iff there's a path from src to dest.
-     * DFS concept implementaion (with stack)
-     * @param src
-     * @param dest
-     * @return boolean
-     */
     public boolean path(int src, int dest) {
         node_data cur = _g.getNode(src);
         Stack<node_data> s = new Stack<>();
         HashSet<Integer> visited = new HashSet<>();
         visited.add(src);
         s.add(cur);
-        while(!s.empty()){
+        while (!s.empty()) {
             cur = s.pop();
-            for(edge_data d: _g.getE(cur.getKey())){
+            for (edge_data d : _g.getE(cur.getKey())) {
                 node_data next = _g.getNode(d.getDest());
-                if(!visited.contains(next.getKey())){
+                if (!visited.contains(next.getKey())) {
                     visited.add(next.getKey());
                     s.add(next);
                 }
-                if(next.getKey() == dest)
+                if (next.getKey() == dest)
                     return true;
             }
         }
@@ -121,16 +97,13 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         this.reset();
         node_data destination = _g.getNode(dest);
         _g.getNode(src).setWeight(0);
-        PriorityQueue<node_data> q = new PriorityQueue<>(new Comparator<node_data>(){
+        PriorityQueue<node_data> q = new PriorityQueue<>(new Comparator<node_data>() {
 
-			@Override
-			public int compare(node_data o1, node_data o2) {
-                double delta = o1.getWeight()-o2.getWeight();
-                if(delta > 0) return 1;
-                if(delta < 0) return -1;
-				return 0;
-			}
-            
+            @Override
+            public int compare(node_data o1, node_data o2) {
+                return (int) (o1.getWeight() - o2.getWeight());
+            }
+
         });
         q.add(_g.getNode(src));
         while (!q.isEmpty()) {
@@ -162,11 +135,6 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         }
     }
 
-    
-    /** 
-     * reset for isConnected(). it's also checking for lonely nodes in the graph.
-     * @return boolean
-     */
     private boolean reset_and_lonely_check() {
         for (node_data n : _g.getV()) {
             n.setTag(-1);
@@ -177,15 +145,6 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         return false;
     }
 
-    
-    /**
-     * backtracking after running shortestPathDist().
-     * each node holdes the key exposed it. so starting from dest,
-     * adding to the head of the list the key stored in node.info() recursively untill reaching src.
-     * @param src
-     * @param dest
-     * @return List<node_data>
-     */
     @Override
     public List<node_data> shortestPath(int src, int dest) {
         if (this.shortestPathDist(src, dest) == -1) {
@@ -201,18 +160,40 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         return res;
     }
 
-    
-    /** 
-     * save the graph in a json format using Gson lib.
-     * @param file
-     * @return boolean
-     */
     @Override
     public boolean save(String file) {
 
-        Gson g = new  GsonBuilder().create();
-        String json = g.toJson(_g);
 
+
+        JsonObject jsonObject = new JsonObject();
+        JsonArray Nodes = new JsonArray();
+        JsonArray Edges = new JsonArray();
+
+        for (node_data n : _g.getV()) {
+            JsonObject node = new JsonObject();
+            if (n.getLocation() != null) {
+                String loc = n.getLocation().x() + "," + n.getLocation().y() + "," + n.getLocation().z();
+                node.addProperty("pos", loc);
+                node.addProperty("id", n.getKey());
+                Nodes.add(node);
+            }
+
+            for (edge_data e : _g.getE(n.getKey())) {
+            JsonObject edge = new JsonObject();
+            if (e != null){
+                    edge.addProperty("src",e.getSrc());
+                edge.addProperty("w",e.getWeight());
+                edge.addProperty("dest",e.getDest());
+                }
+                Edges.add(edge);
+            }
+        }
+        jsonObject.add("Edges",Edges);
+        jsonObject.add("Nodes",Nodes);
+
+
+        Gson g = new  GsonBuilder().create();
+         String json = g.toJson(jsonObject);
         try {
             PrintWriter pw = new PrintWriter(new File(file));
             pw.write(json);
@@ -225,26 +206,19 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         }
     }
 
-    
-    /** 
-     * loads a graph from json test using Gson lib.
-     * @param file
-     * @return boolean
-     */
     @Override
     public boolean load(String file) {
 
         try {
             GsonBuilder builder = new GsonBuilder();
-            builder.registerTypeAdapter(directed_weighted_graph.class, new JsonGraph() );
+            builder.registerTypeAdapter(directed_weighted_graph.class, new JsonGraph());
             Gson gson = builder.create();
 
             FileReader reader = new FileReader((file));
-            _g = gson.fromJson(reader,directed_weighted_graph.class);
+            _g = gson.fromJson(reader, directed_weighted_graph.class);
 
-         return true;
-        }
-        catch (IOException e) {
+            return true;
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
