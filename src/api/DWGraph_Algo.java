a819edbbc5eb51466d31c03a89a8a73825f33900
RavidSaadia <ravids1233@gmail.com>
2020-12-18 12:29:36 +0200

    final code version

--- src/api/DWGraph_Algo.java
+++ src/api/DWGraph_Algo.java
@@ -12,21 +12,39 @@ public class DWGraph_Algo implements dw_graph_algorithms {
 
     private directed_weighted_graph _g;
 
+    /**
+     * init target graph
+     * @param g
+     */
     @Override
     public void init(directed_weighted_graph g) {
         _g = g;
     }
 
+    /**
+     * returns the initialized graph
+     * @return directed_weighted_graph
+     */
     @Override
     public directed_weighted_graph getGraph() {
         return _g;
     }
 
+    /**
+     * perform a deep copy of the graph by using NodeData and DWGraph_DS copy constructors
+     * and basically building a new graph from scratch.
+     * @return directed_weighted_graph
+     */
     @Override
     public directed_weighted_graph copy() {
         return new DWGraph_DS(_g);
     }
 
+    /**
+     * running BFS but each edge u,v we check if there's a path back from v to u
+     * (not necessarily the shortest one)
+     * @return boolean
+     */
     @Override
     public boolean isConnected() {
         if (_g.nodeSize() == 0 || _g.nodeSize() == 1) {
@@ -35,11 +53,11 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         if (this._g.nodeSize() > this._g.edgeSize() + 1) { //the minimum number of edges for a connected graph, shallow chek.
             return false;
         }
-        if (reset_and_lonely_check()) {//reset all the Tags in the graph to 0 and check if there is a lonely node.
+        if (reset_and_lonely_check()) {//reset all the Tags in the graph to -1 and check if there is a lonely node.
             return false;            // if there is a lonely node return false immediately.
         }
 
-        Queue<node_data> q = new LinkedList<>(); // Queue to store all the checked Nodes.
+        Queue<node_data> q = new LinkedList<>();
         q.add(_g.getV().iterator().next());
         int counter = 0;
         node_data cur = q.peek();
@@ -48,7 +66,7 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         while (!q.isEmpty()) {
             cur = q.poll();
             counter++;
-            for (edge_data i : _g.getE(cur.getKey())) { //check if V neighbor has been checked.
+            for (edge_data i : _g.getE(cur.getKey())) {
                 node_data dest = _g.getNode(i.getDest());
                 if (dest.getTag() == -1) {
                     if (!path(dest.getKey(), cur.getKey())) {
@@ -61,6 +79,14 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         }
         return _g.getV().size() == counter;
     }
+
+    /**
+     * returns true iff there's a path from src to dest.
+     * DFS concept implementation (with stack)
+     * @param src
+     * @param dest
+     * @return boolean
+     */
 
     public boolean path(int src, int dest) {
         node_data cur = _g.getNode(src);
@@ -97,13 +123,15 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         this.reset();
         node_data destination = _g.getNode(dest);
         _g.getNode(src).setWeight(0);
-        PriorityQueue<node_data> q = new PriorityQueue<>(new Comparator<node_data>() {
+        PriorityQueue<node_data> q = new PriorityQueue<>(new Comparator<node_data>(){
 
             @Override
             public int compare(node_data o1, node_data o2) {
-                return (int) (o1.getWeight() - o2.getWeight());
+                double delta = o1.getWeight()-o2.getWeight();
+                if(delta > 0) return 1;
+                if(delta < 0) return -1;
+                return 0;
             }
-
         });
         q.add(_g.getNode(src));
         while (!q.isEmpty()) {
@@ -135,6 +163,10 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         }
     }
 
+    /**
+     * reset for isConnected(). it's also checking for lonely nodes in the graph.
+     * @return boolean
+     */
     private boolean reset_and_lonely_check() {
         for (node_data n : _g.getV()) {
             n.setTag(-1);
@@ -145,6 +177,14 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         return false;
     }
 
+    /**
+     * backtracking after running shortestPathDist().
+     * each node holdes the key exposed it. so starting from dest,
+     * adding to the head of the list the key stored in node.info() recursively untill reaching src.
+     * @param src
+     * @param dest
+     * @return List<node_data>
+     */
     @Override
     public List<node_data> shortestPath(int src, int dest) {
         if (this.shortestPathDist(src, dest) == -1) {
@@ -160,11 +200,13 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         return res;
     }
 
+    /**
+     * save the graph in a json format using Gson lib.
+     * @param file
+     * @return boolean
+     */
     @Override
     public boolean save(String file) {
-
-
-
         JsonObject jsonObject = new JsonObject();
         JsonArray Nodes = new JsonArray();
         JsonArray Edges = new JsonArray();
@@ -206,6 +248,11 @@ public class DWGraph_Algo implements dw_graph_algorithms {
         }
     }
 
+    /**
+     * loads a graph from json test using Gson lib.
+     * @param file
+     * @return boolean
+     */
     @Override
     public boolean load(String file) {
 
