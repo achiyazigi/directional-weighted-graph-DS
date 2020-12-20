8241c0d74e78cf830eef9a4d402382656595c27f
achiyazigi <matanelcohen0@walla.com>
2020-12-20 18:22:27 +0200

    added ask for id&level

--- src/gameClient/Engine.java
+++ src/gameClient/Engine.java
@@ -1,9 +1,17 @@
 package gameClient;
 
+import java.util.Scanner;
+
 public class Engine {
 
     public static void main(String[] args) throws InterruptedException {
     //    args = new String[]{"316071349","0"};
+        Scanner sc= new Scanner(System.in);
+        if(args.length == 0){
+            System.out.println("enter id and level: ");
+            args = new String[2];
+            args = sc.nextLine().split(" ");
+        }
         Painter painter = new Painter();
         Thread graphics = new Thread(painter);
         graphics.start();
